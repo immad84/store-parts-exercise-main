15ecd4fc787bb8248c24f3bed64b95a5e14ece67
immadyounas <immad.uddin84@gmail.com>
2021-04-23 23:44:48 +0200

    add App test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+it('renders without crash', () => {
+  const div = document.createElement('div');
+  ReactDom.render(<App />, div);
+  ReactDom.unmountComponentAtNode(div);
+})
